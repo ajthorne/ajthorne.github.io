dbd5c7c0b7200ab100586e8baa612dd3b5c7ebc3
Amanda Thorne <manda.thorne@gmail.com>
2016-09-06 12:54:19 -0500

    git settings fix

--- app/scripts/components/Nav.js
+++ app/scripts/components/Nav.js
@@ -7,9 +7,9 @@ const Nav = React.createClass({
       <nav>
         <h2 className="logo-header">AT</h2>
         <ul className="contact-icons">
-          <li><a href="https://github.com/ajthorne"><i className="fa fa-github"></i></a></li>
-          <li><a href="www.linkedin.com/in/amanda-thorne"><i className="fa fa-linkedin-square"></i></a></li>
-          <li><a href="https://fromteachingtocoding.wordpress.com/"><i className="fa fa-wordpress"></i></a></li>
+          <li><a target="_blank" href="https://github.com/ajthorne"><i className="fa fa-github"></i></a></li>
+          <li><a target="_blank" href="https://www.linkedin.com/in/amanda-thorne"><i className="fa fa-linkedin-square"></i></a></li>
+          <li><a target="_blank" href="https://fromteachingtocoding.wordpress.com/"><i className="fa fa-wordpress"></i></a></li>
           <li><a href="mailto: manda.thorne@gmail.com"><i className="fa fa-envelope"></i></a></li>
         </ul>
       </nav>
