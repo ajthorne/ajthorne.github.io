74d34a6f79ad744759a9ffc9723ba10f370c66e5
Amanda Thorne <manda.thorne@gmail.com>
2016-09-06 00:38:24 -0500

    hover effects and stylings

--- app/scripts/components/Work.js
+++ app/scripts/components/Work.js
@@ -8,7 +8,12 @@ const Work = React.createClass({
         <h2 className="work-title">Work</h2>
         <ul className="work-holder">
           <li className="work-item">
-            <a href="http://booknook.surge.sh/"><img src="../../assets/booknook.png"/></a>
+          <div className="hoveritem">
+              <a target="_blank" href="http://booknook.surge.sh/">
+              <span>Book Nook Live Site</span>
+              <img src="../../assets/booknook.png"/>
+              </a>
+            </div>
             <p className="work-copy">
             <span>Book Nook </span>
             An app to support building a literacy community among adolescents. Find new friends, new books to read, and explore an awesome new social media! This was my final project for The Iron Yard and was built using React.js, Node.js, Backbone.js, SASS, HTML5, JavaScript, Kinvey, and Google Books API.
@@ -16,19 +21,27 @@ const Work = React.createClass({
             </p>
           </li>
           <li className="work-item">
-            <a href="http://ajthorne-reactpardy.surge.sh/"><img src="../../assets/reactpardy.png"/></a>
+            <div className="hoveritem">
+              <a target="_blank" href="http://ajthorne-reactpardy.surge.sh/">
+              <span>React-Pardy! Live Site</span>
+              <img src="../../assets/reactpardy.png"/></a>
+            </div>
             <p className="work-copy">
             <span>React-Pardy!</span>
             A Jeopardy clone app. This was built using React.js, Node.js, Backbone.js, SASS, HTML5, JavaScript, and Kinvey.
-            View code on GitHub <a target="_blank" className="gh-work-link2" href="https://github.com/ajthorne/reactopardy">here</a>.</p>
+            View code on GitHub <a target="_blank" className="gh-work-link" href="https://github.com/ajthorne/reactopardy">here</a>.</p>
           </li>
           <li className="work-item">
-            <img src="../../assets/bringtheband.png"/>
-            <p className="work-copy">
-            <span>Bring the Band</span>
-            A search and voting app built to manipulate data rendered from Spotify's API. This was built using React.js, Node.js, Backbone.js, SASS, HTML5, JavaScript, and Kinvey.
-            View code on GitHub <a target="_blank" className="gh-work-link3" href="https://github.com/ajthorne/bring-the-band">here</a>.
-            </p>
+            <div className="hoveritem">
+              <a target="_blank" href="http://ajthorne-reactpardy.surge.sh/">
+              <span>Bring the Band Live Site</span>
+              <img src="../../assets/bringtheband.png"/></a>
+            </div>
+              <p className="work-copy">
+              <span>Bring the Band</span>
+              A search and voting app built to manipulate data rendered from Spotify's API. This was built using React.js, Node.js, Backbone.js, SASS, HTML5, JavaScript, and Kinvey.
+              View code on GitHub <a target="_blank" className="gh-work-link" href="https://github.com/ajthorne/bring-the-band">here</a>.
+              </p>
           </li>
         </ul>
       </section>
